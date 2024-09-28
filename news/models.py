c75508b796d8f4a19424f8aa81f984baaad7b986
Kieran Cattermole <kctffs@gmail.com>
2024-09-28 20:48:30 +0000

    created uploads model, migrated model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Uploads(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_uploads")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
