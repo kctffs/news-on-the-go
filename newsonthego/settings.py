7539f116d24f9770b98669eb8ff1e4dc623638f4
Kieran Cattermole <kctffs@gmail.com>
2024-09-28 20:10:02 +0000

    secret key for database secruity

--- newsonthego/settings.py
+++ newsonthego/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7g4o4(^cnmh86o0d3g#r@pu=nhri0&0l_s9_eixi7tzhibwbw&'
+SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
