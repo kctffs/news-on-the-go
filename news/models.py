62da75c275b28217297cb1bcad92e45bf3ef7c0d
Kieran Cattermole <kctffs@gmail.com>
2024-09-28 23:05:31 +0000

    created commenting model

--- news/models.py
+++ news/models.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 class Uploads(models.Model):
     title = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_uploads")
+    journalist = models.ForeignKey(User, on_delete=models.CASCADE, related_name="news_uploads")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     excerpt = models.TextField(blank=True)
     
+class Commenting(models.Model):
+    comment = models.ForeignKey(Uploads, on_delete=models.CASCADE, related_name="comments")
+    journalist = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    authorised = models.BooleanField(default=False)
