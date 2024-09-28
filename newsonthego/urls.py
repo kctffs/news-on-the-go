135d473407068f207158053415868bcb70102b1a
Kieran Cattermole <kctffs@gmail.com>
2024-09-28 23:50:18 +0000

    created news urls, and news templates dir

--- newsonthego/urls.py
+++ newsonthego/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from news.views import news_story
+from django.urls import path, include
+
 
 urlpatterns = [
+    path("", include("news.urls"), name="news-urls"),
     path('admin/', admin.site.urls),
-    path('news/', news_story, name='news'),
 ]
