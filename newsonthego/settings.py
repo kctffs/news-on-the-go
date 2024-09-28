fa60fe26497e4b5854ee1ae57c00a3e95a15fa82
Kieran Cattermole <kctffs@gmail.com>
2024-09-28 20:57:29 +0000

    corrected imports for Uploads models

--- newsonthego/settings.py
+++ newsonthego/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kctffs-newsonthego-rrk90b3a82c.ws-eu116.gitpod.io',
                 '.herokuapp.com']
@@ -90,6 +90,13 @@ WSGI_APPLICATION = 'newsonthego.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com",
+    "https://8000-kctffs-newsonthego-rrk90b3a82c.ws-eu116.gitpod.io"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
