57d75bfedb82788ce6c9c3b36a8987b2a365dc3a
Kieran Cattermole <kctffs@gmail.com>
2024-09-29 11:22:46 +0000

    admin display

--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Uploads
 from .models import Uploads, Commenting
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Uploads)
+class UploadsAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'author')
+    search_fields = ['title']
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Uploads)
 admin.site.register(Commenting)
