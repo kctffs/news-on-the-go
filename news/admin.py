6f15001082b716409d5944f5d0ceffbd61960525
Kieran Cattermole <kctffs@gmail.com>
2024-09-29 11:26:30 +0000

    further additions to admin panel

--- news/admin.py
+++ news/admin.py
@@ -6,8 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Uploads)
 class UploadsAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'author')
-    search_fields = ['title']
+    list_display = ('title', 'slug', 'author', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
