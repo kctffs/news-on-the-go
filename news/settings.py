155d137465a67f3709455a751df9a82bb6b30cb4
kctffs <155545578+kctffs@users.noreply.github.com>
2025-04-23 13:33:44 +0000

    update database connection string for production environment

--- news/settings.py
+++ news/settings.py
@@ -2,7 +2,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgres://uoinxdgnoet:bSYLayxSz7Ox@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech:5432/mousy_stony_snarl_880290',
+        default='postgresql://neondb_owner:npg_WEDxlNU3Hy1S@ep-proud-grass-a2f34ebo.eu-central-1.aws.neon.tech/date_outer_pod_313966',
         conn_max_age=600,
         ssl_require=True  # Enforce SSL
     )
